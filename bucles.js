8c0e00978dbc9a2bd90bda63c33f0f1f5aefd64f
David-Stodola <eliasstodola@gmail.com>
2023-09-14 19:26:59 -0300

    Introcuccion a do-while v.p

--- bucles.js
+++ bucles.js
@@ -1,1 +1,80 @@
-console.log(`bucleWhile en Js`)
+////console.log(`bucleWhile en Js`)
+/////document.write('Hola desde document write <br>'); asegurar a que sea falso porque sino queda en un bucle infinito. Y chrome se satura
+///condiciones true 
+
+///let iteraciones = 0 /30 / 31
+
+///while (iteraciones < 30) {
+    //document.write(`Hola desde document write <br>`);
+    //iteraciones = iteraciones +1
+    
+}
+
+//document.write(`luego del while`)
+
+// es mejor usar una variable tiene menos errores, por ej mejor que lo elija mejor un while menor o igual . ≤
+
+
+
+///let iteraciones = 1;
+
+///while (iteraciones < 30) {
+   /// document.write(`${iteraciones} - Hola desde document write <br>`);
+   /// iteraciones = iteraciones + 1;
+    
+//}
+
+//document.write(`luego del while`)
+
+
+///Pasando por todos los nnumeros hata llegar al 576 inclusive quiero que me inpriman en el doc todos los numeros pares
+//pregunto si el numero que estoy iterando es multiplo hago  un doc wr en el doc.
+//indicando que el numero X es multiplo de 2.
+
+
+//let iteraciones = 1;
+//let numeroMaximo = 176;
+
+
+
+///while (iteraciones ≤ numeromaximo) {
+    if (iteraciones % 2 == 0)
+    document.write(`${iteraciones} - Hola desde document write <br>`);
+    iteraciones = iteraciones + 2;
+    
+//}
+
+
+
+
+
+
+
+
+
+
+
+///while (iteraciones ≤ 176) {
+   /// if (iteraciones % 2 == 0) {
+       // document.write(`${iteraciones} - Es divisible por 2 <br>`);
+      //  iteraciones = iteraciones + 2;
+    
+   // }
+    
+ ///   iteraciones++
+}
+
+// while(numero ≤ numeroMaximo0) { bla bla bla}   *(ojo con esto porque puede quedar en bucle inf)
+
+
+
+
+///martin
+while (numero≤ numeroMaximo) {
+    if (numero % 2 === 0) {
+        document.write(`el numero ${numero} es multiplo de <br>`)
+    }
+}
+    
+numero++
+//pasando a 176 
---
+++ do-while.js
@@ -0,0 +1,21 @@
+///const estacion = prompt("Ingrese una estacionn del ano").toLocaleLowerCase();
+
+///while () {
+    
+    //Le vuelvo a pedir que haga un prompt 
+    ///estacion = prompt("Ingrese una estacion del ano").toLocaleLowerCase;
+///}
+
+
+//const estacion = prompt("Ingrese una estacionn del ano").toLocaleLowerCase();
+
+let edadCliente = parseInt(prompt("Ingrese edad del cliente"));
+
+while (isNaN(edadCliente || edadCliente < 120  <+0 || )) {
+    
+    //le vuelvo a pedir que haga un prompt
+    edadCliente = parseInt(prompt("Ingrese edad de cliente"));
+    
+}
+
+console.log(edadCliente)
